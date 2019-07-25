7eb0691e55321d5fb2a45b3e56bdc4c33b0935a0
chhan <chhan@redhat.com>
2019-07-26 00:09:36 +0800

    function view

--- typeidea/config/models.py
+++ typeidea/config/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.template.loader import render_to_string
 
 
 # Create your models here.
@@ -30,17 +31,22 @@ class Link(models.Model):
 
 
 class SideBar(models.Model):
-    STATUS_NORMAL = 1
-    STATUS_DELETE = 0
+    STATUS_SHOW = 1
+    STATUS_HIDE = 0
     STATUS_ITEMS = (
-        (STATUS_NORMAL, '正常'),
-        (STATUS_DELETE, '删除'),
+        (STATUS_SHOW, '展示'),
+        (STATUS_HIDE, '隐藏'),
     )
+
+    DISPLAY_HTML = 1
+    DISPLAY_LATEST = 2
+    DISPLAY_HOT = 3
+    DISPLAY_COMMENT = 4
     SIDE_TYPE = (
-        (1, 'HTML'),
-        (2, '最新文章'),
-        (3, '最热文章'),
-        (4, '最近评论'),
+        (DISPLAY_HTML, 'HTML'),
+        (DISPLAY_LATEST, '最新文章'),
+        (DISPLAY_HOT, '最热文章'),
+        (DISPLAY_COMMENT, '最近评论'),
     )
 
     title = models.CharField(max_length=50, verbose_name="标题")
@@ -56,7 +62,7 @@ class SideBar(models.Model):
         help_text="如果设置的不TML是类，可为空",
     )
     status = models.PositiveIntegerField(
-        default=STATUS_NORMAL,
+        default=STATUS_SHOW,
         choices=STATUS_ITEMS,
         verbose_name="状态")
     owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.CASCADE)
@@ -67,3 +73,36 @@ class SideBar(models.Model):
 
     def __str__(self):
         return self.title
+
+    @classmethod
+    def get_all(cls):
+        return cls.objects.filter(status=cls.STATUS_SHOW)
+
+    @property
+    def content_html(self):
+        """
+        直接渲染模板
+        :return:
+        """
+        from blog.models import Post
+        from comment.models import Comment
+
+        result = ''
+        if self.display_type == self.DISPLAY_HTML:
+            return self.content
+        elif self.display_type == self.DISPLAY_LATEST:
+            context = {
+                'posts': Post.latest_posts()
+            }
+            result = render_to_string('config/blocks/sidebar_posts.html', context)
+        elif self.display_type == self.DISPLAY_HOT:
+            context = {
+                'posts': Post.hot_posts()
+            }
+            result = render_to_string('config/blocks/sidebar_posts.html', context)
+        elif self.display_type == self.DISPLAY_COMMENT:
+            context = {
+                'comments': Comment.objects.filter(status=Comment.STATUS_NORMAL)
+            }
+            result = render_to_string('config/blocks/sidebar_comments.html', context)
+        return result
