a5b9f867e81cd1c206bbd1d6c6ead01b23446c86
chhan <chhan@redhat.com>
2019-07-29 13:56:18 +0800

    class-based-view, bootstrap, search and author

--- typeidea/config/views.py
+++ typeidea/config/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 from django.http import HttpResponse
+from blog.views import CommonViewMixin
+from config.models import Link
 
 # Create your views here.
-def links(request):
-    return HttpResponse('links')
+# def links(request):
+#     return HttpResponse('links')
+
+
+class LinkListView(CommonViewMixin, ListView):
+    queryset = Link.objects.filter(status=Link.STATUS_NORMAL)
+    template_name = 'config/links.html'
+    context_object_name = 'link_list'
